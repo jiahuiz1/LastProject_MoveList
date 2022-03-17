acaae6a04015b90bb4c1d2ee348855353a2c1987
jiahuiz1 <46904850+jiahuiz1@users.noreply.github.com>
2022-03-16 19:08:54 -0700

    Movie List page (#2)
    * Movie List page
    
    Finish the basic design of movie list page
    
    * add router for all pages provided by Tao
    
    fix the issue that navigation between pages does not work
    
    * Rename Movie.js to movie.js

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,27 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './containers/home/home';
+import MovieList from './containers/movieList/movieList';
+import MovieLikeList from './containers/movieLikeList/movieLikeList';
+import MovieBlockList from './containers/movieBlockList/movieBlockList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Bootstrap</Button>
-      </header>
+      <Router>
+        <header>
+          <h1>Top Rated Movies List</h1>
+        </header>
+        <div className="nav-container-menu">
+          <nav className="nav-container-menu-items">
+              <Link className="btn btn-primary btn-lg" role="button" to="/">
+                Home
+              </Link>
+              <Link
+                className="btn btn-primary btn-lg"
+                role="button"
+                to="/movieList"
+              >
+                Movie List
+              </Link>
+              <Link
+                className="btn btn-primary btn-lg"
+                role="button"
+                to="/movieBlockList"
+              >
+                Movie Block List
+              </Link>
+              <Link
+                className="btn btn-primary btn-lg"
+                role="button"
+                to="/movieLikeList"
+              >
+                Movie Like List
+              </Link>
+          </nav>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/movieList" element={<MovieList/>}/>
+          <Route path="/movieBlockList" element={<MovieBlockList/>}/>
+          <Route path="/movieLikeList" element={<MovieLikeList/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
